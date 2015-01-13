b162742ac4bfefd5eb2509ef637ceee6055430f7
shellscape <andrew@shellscape.org>
2015-01-13 10:45:33 -0500

    adding inline config capability

--- index.js
+++ index.js
@@ -1,125 +1,159 @@
 'use strict';
-var gutil = require('gulp-util');
-var through = require('through2');
-var chalk = require('chalk');
-var recess = require('recess');
-var RcLoader = require('rcloader');
+var gutil = require('gulp-util'),
+	through = require('through2'),
+	chalk = require('chalk'),
+	recess = require('recess'),
+	RcLoader = require('rcloader'),
+	pluginName = 'gulp-recess-plus';
+
+/**
+ * Loads inline options from css/less files.
+ */
+function loadInlineOptions (options, contents) {
+
+  if (/\/\*\s?recess/.test(contents)) {
+    var matches = contents.match(/\/\*\s?recess(.+?)\*\/|\/\/.*/),
+    	pairs;
+
+    if (matches.length > 1) {
+    	// we could split on ',', but this is stricter
+      pairs = matches[1].trim().match(/\w+(\s+)?:(\s+)?\w+(?=,?\s?)/g);
+
+      // todo: improve the regex so this isn't needed
+      pairs.forEach(pairs, function (part) {
+        var bits = part.split(':'),
+          key = bits[0].trim(),
+          value = bits[1].trim();
+
+        /* jshint evil: true */
+        options[key] = eval(value);
+      });
+    }
+  }
+
+  return options;
+}
 
 // prevent RECESS from reading files
 recess.Constructor.prototype.read = function () {
-	setImmediate(function () {
-		this.data = this.options.contents;
-		this.parse();
-	}.bind(this));
+  setImmediate(function () {
+    this.data = this.options.contents;
+    this.parse();
+  }.bind(this));
 };
 
 module.exports = function (options) {
-	options = options || {};
+  options = options || {};
 
-	var rcLoader = new RcLoader('.recessrc', options);
+  var rcLoader = new RcLoader('.recessrc', options),
+  	contents;
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-recess', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new gutil.PluginError(pluginName, 'Streaming not supported'));
+      return;
+    }
 
-		options = rcLoader.for(file.path);
-		options.contents = file.contents.toString();
+    contents = file.contents.toString();
 
-		recess(file.path, options, function (err, results) {
-			if (err) {
-				err.forEach(function (el) {
-					var realError = new Error(el.message);
+    options = rcLoader.for(file.path);
+    options = loadInlineOptions(options, contents);
+    options.contents = contents;
 
-					// el is an instance of LessError, which does not inherit
-					// from Error. PluginError expects an instance of Error.
-					// create a real Error and map LessError properties to it.
-					realError.columnNumber = el.column;
-					realError.fileName = el.filename;
-					realError.lineNumber = el.line;
-					realError.stack = el.stack;
-					realError.type = el.name;
+    recess(file.path, options, function (err, results) {
+      if (err) {
+        err.forEach(function (el) {
+          var realError = new Error(el.message);
 
-					var recessError = new gutil.PluginError('gulp-recess', realError, {
-						showStack: false
-					});
+          // el is an instance of LessError, which does not inherit
+          // from Error. PluginError expects an instance of Error.
+          // create a real Error and map LessError properties to it.
+          realError.columnNumber = el.column;
+          realError.fileName = el.filename;
+          realError.lineNumber = el.line;
+          realError.stack = el.stack;
+          realError.type = el.name;
 
-					recessError.recess = {
-						message: el.message,
-						filename: el.filename,
-						line: el.line,
-						col: el.column
-					};
+          var recessError = new gutil.PluginError(pluginName, realError, {
+            showStack: false
+          });
 
-					this.emit('error', recessError);
-				}, this);
-				this.push(file);
-				return;
-			}
+          recessError.recess = {
+            message: el.message,
+            filename: el.filename,
+            line: el.line,
+            col: el.column
+          };
 
-			var data = results[0]; // we only linted one file
-			var failureCount = 0;
+          this.emit('error', recessError);
+        }, this);
+        this.push(file);
+        return;
+      }
 
-			if (data.output.length > 2) {
-				var match = /([0-9]+) fail/i.exec(chalk.stripColor(data.output[2]));
+      var data = results[0], // we only linted one file
+      	failureCount = 0,
+      	match;
 
-				if (match) {
-					failureCount = parseInt(match[1]);
-				}
-			}
+      if (data.output.length > 2) {
+        match = /([0-9]+) fail/i.exec(chalk.stripColor(data.output[2]));
 
-			file.recess = {
-				success: (data.output[1].indexOf('Busted') === -1),
-				status: chalk.stripColor(data.output[1]).replace(/status: /i,'').trim(),
-				failureCount: failureCount,
-				results: data.output.slice(3),
-				errors: data.errors,
-				opt: options
-			};
+        if (match) {
+          failureCount = parseInt(match[1]);
+        }
+      }
 
-			cb(null, file);
-		}.bind(this));
-	});
+      file.recess = {
+        success: (data.output[1].indexOf('Busted') === -1),
+        status: chalk.stripColor(data.output[1]).replace(/status: /i,'').trim(),
+        failureCount: failureCount,
+        results: data.output.slice(3),
+        errors: data.errors,
+        opt: options
+      };
+
+      cb(null, file);
+    }.bind(this));
+});
 };
 
 module.exports.reporter = function (options) {
-	options = options || {};
+  options = options || {};
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-recess', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new gutil.PluginError(pluginName, 'Streaming not supported'));
+      return;
+    }
 
-		var recessDataForThisFile = file.recess;
+    var recessDataForThisFile = file.recess;
 
-		if (recessDataForThisFile && !recessDataForThisFile.success) {
-			console.log(' [' + chalk.green('gulp-recess') + '] ' + file.relative + ': ' + chalk.red(recessDataForThisFile.status) + ' ' + recessDataForThisFile.failureCount+' failures');
+    if (recessDataForThisFile && !recessDataForThisFile.success) {
+      console.log(' [' + chalk.green(pluginName) + '] ' + file.relative + ': ' + chalk.red(recessDataForThisFile.status) + ' ' + recessDataForThisFile.failureCount+' failures');
 
-			if (!options.minimal) {
-				console.log(file.recess.results.join('\n'));
-			}
+      if (!options.minimal) {
+        console.log(file.recess.results.join('\n'));
+      }
 
-			if (!options.hasOwnProperty('fail') || options.fail) {
-				cb(new gutil.PluginError('gulp-recess', file.relative + ': ' + file.recess.status + ' ' + recessDataForThisFile.failureCount + ' failures', {
-					fileName: file.path,
-					showStack: false
-				}));
-				return;
-			}
-		}
+      if (!options.hasOwnProperty('fail') || options.fail) {
+        cb(new gutil.PluginError(pluginName, file.relative + ': ' + file.recess.status + ' ' + recessDataForThisFile.failureCount + ' failures', {
+          fileName: file.path,
+          showStack: false
+        }));
+        return;
+      }
+    }
 
-		cb(null, file);
-	});
+    cb(null, file);
+  });
 };
